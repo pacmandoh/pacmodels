0e9557e2d5746cd6851e72f8cf649a5f2b16c318
pacmandoh <pacmandoh@outlook.com>
2024-03-23 20:43:42 +0800

    chore(deps): update * feat: add nuxt/ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
     '@vueuse/nuxt',
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxt/ui'
   ]
 })
